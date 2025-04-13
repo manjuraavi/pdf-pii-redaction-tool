70d24d55b0943a745ae605330063319eade4b43f
Manjusha Raavi <manjuraavi24@gmail.com>
2025-04-13 14:51:00 +0530

    feat: added Streamlit to install_requires for web interface support

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        "streamlit",
+        "pandas",
         "openai>=1.0.0",
         "langdetect",
         "python-dotenv",
